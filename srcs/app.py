b9031f20a6900460ee040772fb25e21c27dfe256
wz1114841863 <18801383735@163.com>
2025-10-20 10:58:50 +0800

    add app.py

---
+++ srcs/app.py
@@ -0,0 +1,65 @@
+import sys
+import streamlit as st
+
+import config
+import search_engine
+
+from pathlib import Path
+
+# To enable app.py to locate and import our own module
+APP_DIR = Path(__file__).parent
+sys.path.append(str(APP_DIR))
+
+# Streamlit Page Configuration
+st.set_page_config(
+    page_title="Litmus 🧪 - Your Personal Research Assistant",
+    page_icon="🧪",
+    layout="wide",  # use wide mode for more space
+)
+
+st.title("Litmus 🧪")
+st.markdown(
+    """
+        Welcome to Litmus, your personal AI-powered research assistant!
+        Upload academic papers in PDF format, and let Litmus help you analyze and search through them with ease.
+    """
+)
+st.markdown("---")
+
+query = st.text_input(
+    "Enter your search keywords or query:",
+    placeholder="e.g., Transformer architecture in NLP",
+)
+search_button = st.button("Search")
+
+if search_button and query:
+    with st.spinner(
+        "Searching through your knowledge base... (this may take a moment)"
+    ):
+        results = search_engine.hybrid_search(query)
+
+    st.subheader(f'Found {len(results)} relevant papers for: "{query}"')
+
+    if not results:
+        st.warning(
+            "No papers found that meet the relevance threshold. "
+            f"You could try a different query or adjust the `SEMANTIC_SEARCH_THRESHOLD` in `config.py` (current value: {config.SEMANTIC_SEARCH_THRESHOLD})."
+        )
+    else:
+        for paper in results:
+            with st.expander(
+                f"**{paper['title']}** ({paper['conference']} {paper['year']})"
+            ):
+                st.markdown(f"**Authors:** *{paper['authors']}*")
+                st.info(f"**AI Summary:** {paper['generated_summary']}")
+                st.markdown(f"**Keywords:** `{paper['keywords']}`")
+                # create a google search link for the paper title
+                google_search_url = f"https://www.google.com/search?q={paper['title'].replace(' ', '+')}"
+                st.markdown(
+                    f"[Search for this paper on Google]({google_search_url})",
+                    unsafe_allow_html=True,
+                )
+
+                st.markdown(f"--- \n *Local Path: `{paper['file_path']}`*")
+elif search_button and not query:
+    st.warning("Please enter a search query to proceed.")
